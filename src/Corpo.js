ebf0f9c532ca678c74a51137a84d9e98b119717b
Guilherme Silva <102394141+guilhermegssilva@users.noreply.github.com>
2022-05-09 16:07:43 -0300

    componentes

---
+++ src/Corpo.js
@@ -0,0 +1,10 @@
+import Esquerda from './Esquerda';
+import Sidebar from './Sidebar';
+export default function Corpo() {
+    return (
+        <div class="corpo">
+            <Esquerda />
+            <Sidebar />
+        </div>
+    );
+}
